18aafabd31ba1fb584f44b471d8e6d38dc457784
Severin <insmer@gmail.com>
2022-02-21 18:38:20 +0500

    add MainPrintReversedParam class

---
+++ src/academy/kovalevskyi/codingbootcamp/week2/day0/MainPrintReversedParam.java
@@ -0,0 +1,15 @@
+package academy.kovalevskyi.codingbootcamp.week2.day0;
+
+public class MainPrintReversedParam {
+
+    public static void main(String[] args) {
+        int len = args.length;
+        if (len == 0) {
+            System.out.println("Please specify at least one argument!");
+            return;
+        }
+        for (int i = 0; i < len ; i++) {
+            System.out.println(len - 1 - i);
+        }
+    }
+}
