8af4611874e16c1ea89046c6df0bd87756c38cb8
Severin <insmer@gmail.com>
2022-02-21 18:59:12 +0500

    optimate Sorting.java

--- src/academy/kovalevskyi/codingbootcamp/week1/day1/Sorting.java
+++ src/academy/kovalevskyi/codingbootcamp/week1/day1/Sorting.java
@@ -5,6 +5,9 @@ import java.util.Comparator;
 public class Sorting {
 
     public static <T> void sort(T[] target, Comparator<T> comparator) {
+        if (target == null || target.length == 0) {
+            throw new NullPointerException("array is emptyi");
+        }
         T temp;
         for (int i = target.length; i >= 0; i--) {
             for (int j = 0; j < i - 1; j++) {
@@ -18,15 +21,7 @@ public class Sorting {
     }
 
     public static <T> void sortReversedOrder(T[] target, Comparator<T> comparator) {
-        T temp;
-        for (int i = target.length; i >= 0; i--) {
-            for (int j = 0; j < i - 1; j++) {
-                if (comparator.compare(target[j], target[j + 1]) < 0) {
-                    temp = target[j];
-                    target[j] = target[j + 1];
-                    target[j + 1] = temp;
-                }
-            }
-        }
+        sort(target, comparator.reversed());
     }
+
 }
