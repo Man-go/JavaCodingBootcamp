39bac05c81c6ca50d082f5803ebbec2db759f764
Severin <insmer@gmail.com>
2022-02-22 16:10:28 +0500

    add MainPrintSortedParam.java fix

--- src/academy/kovalevskyi/codingbootcamp/week2/day0/MainPrintSortedParam.java
+++ src/academy/kovalevskyi/codingbootcamp/week2/day0/MainPrintSortedParam.java
@@ -1,11 +1,17 @@
 package academy.kovalevskyi.codingbootcamp.week2.day0;
 
 import academy.kovalevskyi.codingbootcamp.week1.day1.Sorting;
-import java.util.Comparator;
 
 public class MainPrintSortedParam {
 
     public static void main(String[] args) {
-        Sorting.sort(args, Comparator.naturalOrder());
+        if (args.length == 0) {
+            System.out.println("Please specify at least one argument!");
+            return;
+        }
+        Sorting.sort(args, String::compareTo);
+        for (String arg : args) {
+            System.out.println(arg);
+        }
     }
 }
